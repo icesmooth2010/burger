3c2fc2e0890315fd4542ddba75f6294774885821
icesmooth2010 <32586621+icesmooth2010@users.noreply.github.com>
2018-05-02 17:39:09 -0600

    big load

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO burgers (task) VALUES ('Pick up milk.');
+INSERT INTO burgers (task) VALUES ('Mow the lawn.');
+INSERT INTO burgers (task) VALUES ('Call Shannon back.');
+
+INSERT INTO burgers(burger_name, devoured,)
+VALUES ("Big Mac", false),
+  ("Whooper", false),
+  ("Cheezburger", false),
+  ("Baconator", false),
+  ("Quarter Pounder", false),
+  ("McRoyal", false),
+  ("Double Double", true);
